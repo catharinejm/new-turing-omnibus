769236de0096def78bd4a2bd2316d17d23f97c14
Jon Distad <jon.distad@gmail.com>
2012-07-22 04:27:45 -0400

    Adding inputs

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject new-turing-omnibus "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.4"]]
+  :plugins [[lein-cljsbuild "0.2.4"]
+            [lein-swank "1.4.4"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:output-to "main.js"
                                    :output-dir "out"}}]})
--- src/ch1/wallpaper.cljs
+++ src/ch1/wallpaper.cljs
@@ -1,5 +1,11 @@
 (ns ch1.wallpaper
-  (:require [goog.graphics :as gfx]))
+  (:require [goog.graphics :as gfx]
+            [goog.dom :as dom]))
+
+(extend-type js/NodeList
+  cljs.core.ISeqable
+  (-seq [this] (doall (map #(.item this %)
+                           (range (.-length this))))))
 
 (def game-window (gfx/createGraphics 800 600))
 (def stroke (gfx/Stroke. 0 "#FFF"))
@@ -8,9 +14,36 @@
 (defn draw-point! [canvas x y]
   (.drawRect canvas x y 1 1 stroke fill))
 
-(draw-point! game-window 10 10)
-(draw-point! game-window 20 10)
-(draw-point! game-window 30 10)
+(defn wallpaper! [canvas corna cornb side]
+  (dotimes [i 100]
+    (dotimes [j 100]
+      (let [x (+ i 8)
+            y (+ j 6)]
+        (when (even? (+ (* x x) (* y y)))
+          (draw-point! canvas i j))))))
+
+(defn serialize-inputs [form-el]
+  (let [inputs (.querySelectorAll form-el "input")]
+    (reduce (fn [m el]
+              (assoc m
+                (keyword (.getAttribute el "name"))
+                (.-value el)))
+            {} inputs)))
+
+(defn nan? [x]
+  (and (number? x) (not= x x)))
+
+(defn update-canvas [canvas form-el event]
+  (let [inputs (serialize-inputs form-el)
+        int-inputs (map (comp js/parseInt inputs) [:corna :cornb :side])]
+    (if (some nan? int-inputs)
+      (js/alert "ETNER SOME NUMMAS")
+      (apply wallpaper! canvas int-inputs))
+    false))
 
 (set! js/window -onload
-      #(.render game-window))
+      (fn []
+        (let [dimensions-form (dom/getElement "dimensions")]
+          (set! dimensions-form -onsubmit
+                (partial update-canvas game-window dimensions-form))
+          (.render game-window))))
